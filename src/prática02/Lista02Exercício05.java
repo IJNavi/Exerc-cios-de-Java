edcf26111b18f69873ebcf70ed99e6607b1d3b10
Ivan Barbosa <ivanjulio.ij@gmail.com>
2024-10-30 09:43:03 -0300

    Melhorias no código do exercício 05, lista 02 da tarefa 02.

--- src/prática02/Lista02Exercício05.java
+++ src/prática02/Lista02Exercício05.java
@@ -14,19 +14,19 @@ public class Lista02Exercício05 {
 
 		System.out.println("# ## ### #### #########   Menu   ########## #### ### ## #");
 
-		System.out.println("\n--	Digite 1 para escolher Cachorro Quente.     --");
-		System.out.println("-- 	Digite 2 para escolher X - Salada.     	    --");
-		System.out.println("--	Digite 3 para escolher X - Bacon.      	    --");
-		System.out.println("--	Digite 4 para escolher Bauru.               --");
-		System.out.println("--	Digite 5 para escolher Refrigerante.        --");
-		System.out.println("--	Digite 6 para escolher Suco de Laranja.     --");
+		System.out.println("\n-- Digite 1 para escolher: Cachorro Quente.   R$10,00 --");
+		System.out.println("-- Digite 2 para escolher: X - Salada.        R$15,00 --");
+		System.out.println("-- Digite 3 para escolher: X - Bacon.         R$18,00 --");
+		System.out.println("-- Digite 4 para escolher: Bauru.             R$12,00 --");
+		System.out.println("-- Digite 5 para escolher: Refrigerante.      R$08,00 --");
+		System.out.println("-- Digite 6 para escolher: Suco de Laranja.   R$13,00 --");
 		System.out.println("\n# ## ### #### #########   Menu   ########## #### ### ## #");		
-		short opcao = leia.nextShort();
+		Byte opcao = leia.nextByte();
 		
 		//Aqui se indica a quantidade.
 		
 		System.out.println("\n\n\n==>> Agora entre com a quantidade do produto selecionado: <<==");
-		short quantidade = leia.nextShort();
+		Byte quantidade = leia.nextByte();
 		
 		//Aqui é feito tanto o processamento e o informe do valor final junto ao informe do nome do produto.
 
